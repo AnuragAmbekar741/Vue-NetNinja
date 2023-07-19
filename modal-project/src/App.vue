42a4622b0110e8abfb8d191029c665a334e3d73c
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2023-07-19 13:47:16 +0530

    modal & VUE cli app

---
+++ modal-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click='handleClick'>Name</button>
+  <Modal/>
+ </div>
+</template>
+
+<script>
+  import Modal from './components/Modal.vue'
+
+  export default{
+    name:'App',
+    components:{Modal},
+    data(){
+      return{
+        title:"First VUE component"
+      }
+    },
+    methods:{
+      handleClick(){
+        console.log(this.$refs.name)
+        this.$refs.name.classList.add('active')
+        this.$refs.name.focus()
+      }
+    }
+  }
+</script>
+
+<style>
+  h1{
+    color: skyblue;
+  }
+</style>
